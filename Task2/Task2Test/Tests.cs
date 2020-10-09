43a4bc73b84f5933abf6d49e3bf92fc092cc10a5
GlassesSimon <33306424+GlassesSimon@users.noreply.github.com>
2020-10-09 22:39:59 +0700

    Task2
    Система книжного магазина.

---
+++ Task2/Task2Test/Tests.cs
@@ -0,0 +1,17 @@
+﻿//using FluentAssertions;
+//using NUnit.Framework;
+//using Task2;
+
+//namespace Task2Test
+//{
+//    [TestFixture]
+//    public class Tests
+//    {
+//        [Test]
+//        public void Test1()
+//        {
+//            var myBookShop = new BookShop();
+//            Assert.True(true);
+//        }
+//    }
+//}
