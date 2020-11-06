f6f37f3a5f39d720d3cac1c6022965ee9ed1423a
Ангелина <GlassesSimon>
2020-11-06 16:19:32 +0700

    Добавила работу с базой.

---
+++ Task2/Task2WebApplication/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Task2;
+using Task2Infrastructure.EntityFramework;
+
+namespace Task2WebApplication.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksContextDbContextFactory _dbContextFactory;
+
+        public BooksController(BooksContextDbContextFactory dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+        [HttpGet]
+        public async Task<List<Book>> GetBooks()
+        {
+            using (var context = _dbContextFactory.GetContext())
+            {
+                return await context.GetBooks();
+            }
+        }
+
+        [HttpPost]
+        public async Task AddBook([FromBody] Book book)
+        {
+            using (var context = _dbContextFactory.GetContext())
+            {
+                context.AddBook(book);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
